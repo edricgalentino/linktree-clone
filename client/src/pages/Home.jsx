63964ca3333b0ad7fdb221b4478b9805ddd12bfe
edricgalentino <erdrikpertama@gmail.com>
2022-06-13 14:56:51 +0700

    asd

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+const Home = () => {
+    return (
+        <React.Fragment>
+            <main>
+                <section className="landing-page container w-full h-screen flex justify-center items-center p-3 ">
+                    <div className="img"></div>
+                    <h1 className="text-4xl font-semibold font-poppins">All of your social profiles, in one page.</h1>
+                </section>
+            </main>
+        </React.Fragment>
+    );
+};
+
+export default Home;
