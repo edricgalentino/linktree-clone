2ff0235342267224793bf0fa2889dc9d431d4e42
edricgalentino <erdrikpertama@gmail.com>
2022-06-15 09:35:52 +0700

    dwa

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -2,80 +2,111 @@ import React, { useEffect, useState } from "react";
 // component
 import NavCreate from "../components/NavCreate";
 import LinktreeLink from "../components/LinktreeLink";
-import DragContainer from "../components/DragContainer";
 import SortableComponent from "../components/SortableComponent";
+import BubblePrototype from "../components/BubblePrototype";
+
+import { BsFillPersonFill } from "react-icons/bs";
+import { GrAdd } from "react-icons/gr";
+import { IoMdColorPalette } from "react-icons/io";
+import { AiFillFormatPainter } from "react-icons/ai";
+import { ImHome3 } from "react-icons/im";
+import Appearrance from "../components/Appearrance";
+import PrototypeMobile from "../components/PrototypeMobile";
+import Theme from "../components/Theme";
 
 const Home = () => {
+    const [linksValue, setLinksValue] = useState([
+        {
+            link: {
+                title: "",
+                link: "https://",
+            },
+        },
+    ]);
+    const [active, setActive] = useState("home");
+    const [titleProfile, setTitleProfile] = useState("");
+    const [descriptionProfile, setDescriptionProfile] = useState("");
+    const [avatarProfile, setAvatarProfile] = useState("");
+
+    const [showInfo, setShowInfo] = useState(false);
     return (
-        <React.Fragment>
+        <>
             <main className="flex justify-end">
-                <NavCreate />
+                <nav className=" lg:w-20 lg:h-96 w-full h-20 rounded-full flex lg:flex-col lg:gap-3 z-50 fixed lg:left-6 lg:top-24 lg:mt-0 mt-96 top-80 lg:justify-center justify-evenly items-center p-5 drop-shadow-lg bg-white">
+                    <button
+                        onClick={() => {
+                            setActive("home");
+                        }}
+                        className={`${active === "home" ? "bg-emerald-200" : ""} icon hover:bg-emerald-400 relative lg:hover:ml-2 transition-all p-3 rounded-full drop-shadow-lg`}
+                    >
+                        <ImHome3 size={"34px"} />
+                    </button>
+                    <button
+                        onClick={() => {
+                            setActive("appearrance");
+                        }}
+                        className={`${active === "appearrance" ? "bg-emerald-200" : ""} icon hover:bg-emerald-400 relative lg:hover:ml-2 transition-all p-3 rounded-full drop-shadow-lg`}
+                    >
+                        <BsFillPersonFill size={"34px"} />
+                    </button>
+                    <button
+                        onClick={() => {
+                            setActive("theme");
+                        }}
+                        className={`${active === "theme" ? "bg-emerald-200" : ""} icon hover:bg-emerald-400 relative lg:hover:ml-2 transition-all p-3 rounded-full drop-shadow-lg`}
+                    >
+                        <IoMdColorPalette size={"34px"} />
+                    </button>
+                    <button
+                        onClick={() => {
+                            setActive("background");
+                        }}
+                        className={`${active === "background" ? "bg-emerald-200" : ""} icon hover:bg-emerald-400 relative lg:hover:ml-2 transition-all p-3 rounded-full drop-shadow-lg`}
+                    >
+                        <AiFillFormatPainter size={"34px"} />
+                    </button>
+                    <button
+                        className={`${active === "home" ? "" : "cursor-not-allowed"} icon bg-emerald-400 relative lg:hover:ml-2 transition-all p-3 rounded-full  hover:drop-shadow-xl`}
+                        onClick={() => {
+                            setLinksValue([
+                                ...linksValue,
+                                {
+                                    link: {
+                                        title: "",
+                                        link: "https://",
+                                    },
+                                },
+                            ]);
+                        }}
+                    >
+                        <GrAdd size={"34px"} color="white" />
+                    </button>
+                </nav>
                 <section className="editor lg:w-11/12 w-full bg-slate-200 flex lg:flex-row flex-col justify-around">
                     <div className="draggable lg:w-2/3 w-full lg:border-r-2 lg:border-b-0 border-b-2 border-black min-h-screen flex justify-center bg-slate-200 items-center">
-                        <div className="container lg:w-2/3 w-11/12 min-h-screen bg-slate-200 flex flex-col gap-4 lg:pb-12 pb-10">
+                        <div className={`${active === "home" ? "" : "hidden"} container home lg:w-2/3 w-11/12 min-h-screen bg-slate-200 flex flex-col gap-4 lg:pb-12 pb-10`}>
                             <LinktreeLink />
-                            <SortableComponent />
+                            <SortableComponent showInfo={showInfo} setShowInfo={(info) => setShowInfo(info)} linksValue={linksValue} setLinksValue={(newLinksValue) => setLinksValue(newLinksValue)} />
+                        </div>
+                        <div className={`${active === "appearrance" ? "" : "hidden"} container appearrance lg:w-2/3 w-11/12 min-h-screen bg-slate-200 flex flex-col gap-4 lg:pb-12 pb-10`}>
+                            <LinktreeLink />
+                            <Appearrance setTitleProfile={(value) => setTitleProfile(value)} setDescriptionProfile={(value) => setDescriptionProfile(value)} />
+                        </div>{" "}
+                        <div className={`${active === "theme" ? "" : "hidden"} container background lg:w-2/3 w-11/12 min-h-screen bg-slate-200 flex flex-col gap-4 lg:pb-12 pb-10`}>
+                            <LinktreeLink />
+                            <Theme />
+                        </div>{" "}
+                        <div className={`${active === "background" ? "" : "hidden"} container theme lg:w-2/3 w-11/12 min-h-screen bg-slate-200 flex flex-col gap-4 lg:pb-12 pb-10`}>
+                            <LinktreeLink />
+                            background
                         </div>
                     </div>
-                    <div className="lg:w-1/3 w-full min-h-screen flex justify-center py-7 flex-col bg-slate-200 items-center">
-                        <div className="resizable w-72 bg-slate-200 font-poppins h-120 overflow-y-auto flex py-7 flex-col justify-between gap-4 items-center border-8 rounded-3xl border-black">
-                            <section class="profile relative flex justify-center flex-col">
-                                <div class="container w-1/3  bg-black border-4 border-black rounded-full mx-auto">
-                                    <img class=" bg-slate-200 rounded-full" src="profile.png" alt="user" />
-                                </div>
-                                <div class="name mt-3">
-                                    <h5 className="text-lg font-semibold">Edric Galentino</h5>
-                                </div>
-                                <div class="desc">
-                                    <p className="text-sm">Front-end Developer</p>
-                                </div>
-                            </section>
-                            <section class="main-content flex justify-center items-center flex-col w-52 gap-3 my-7">
-                                <div class=" bg-white w-full rounded-full p-1 relative hover:ml-2 hover:drop-shadow-xl transition-all">
-                                    <i class="fas fa-globe"></i>
-                                    <a target="_blank" href="https://edricgalentino.github.io" rel="noreferrer">
-                                        <h6 className="text-sm">Portofolio</h6>
-                                    </a>
-                                </div>
-                                <div class=" bg-white w-full rounded-full p-1 relative hover:ml-2 hover:drop-shadow-xl transition-all">
-                                    <i class="fab fa-linkedin"></i>
-                                    <a target="_blank" href="https://linkedin.com/in/edricgalentino" rel="noreferrer">
-                                        <h6 className="text-sm">LinkedIn</h6>
-                                    </a>
-                                </div>
-                                <div class=" bg-white w-full rounded-full p-1 relative hover:ml-2 hover:drop-shadow-xl transition-all">
-                                    <i class="fab fa-github"></i>
-                                    <a target="_blank" href="https://github.com/edricgalentino" rel="noreferrer">
-                                        <h6 className="text-sm">Github</h6>
-                                    </a>
-                                </div>
-                                <div class=" bg-white w-full rounded-full p-1 relative hover:ml-2 hover:drop-shadow-xl transition-all">
-                                    <i class="fab fa-instagram"></i>
-                                    <a target="_blank" href="https://instagram.com/edricgalentino" rel="noreferrer">
-                                        <h6 className="text-sm">Instagram</h6>
-                                    </a>
-                                </div>
-                                <div class=" bg-white w-full rounded-full p-1 relative hover:ml-2 hover:drop-shadow-xl transition-all">
-                                    <i class="fab fa-twitter"></i>
-                                    <a target="_blank" href="https://twitter.com/edricgalentino" rel="noreferrer">
-                                        <h6 className="text-sm">Twitter</h6>
-                                    </a>
-                                </div>
-                                <div class=" bg-white w-full rounded-full p-1 relative hover:ml-2 hover:drop-shadow-xl transition-all">
-                                    <i class="fas fa-coffee"></i>
-                                    <a target="_blank" href="https://ko-fi.com/edricgalentino" rel="noreferrer">
-                                        <h6 className="text-sm">Buy me a coffee!</h6>
-                                    </a>
-                                </div>
-                            </section>
-                            <footer>
-                                <h1 className="text-sm font-semibold">myLinktree</h1>
-                            </footer>
-                        </div>
+                    <div className="lg:w-1/3 w-full min-h-screen flex justify-start lg:pt-44 py-8 flex-col bg-slate-200 items-center">
+                        <PrototypeMobile titleProfile={titleProfile} descriptionProfile={descriptionProfile} linksValue={linksValue} />
                     </div>
                 </section>
             </main>
-        </React.Fragment>
+        </>
     );
 };
 
