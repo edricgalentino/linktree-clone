31d5907ed2bd01be13d549266c1df1b47355a43b
edricgalentino <erdrikpertama@gmail.com>
2022-06-16 11:52:46 +0700

    dwa

--- client/src/components/PrototypeMobile.jsx
+++ client/src/components/PrototypeMobile.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import BubblePrototype from "./BubblePrototype";
 
-function PrototypeMobile({ titleProfile, descriptionProfile, linksValue }) {
+function PrototypeMobile({ titleProfile, descriptionProfile, linksValue, choosenTheme, avatarProfile, dark }) {
     return (
-        <div className="resizable w-72 bg-slate-200 font-poppins h-120 overflow-y-auto flex py-7 flex-col justify-between gap-4 items-center border-8 rounded-3xl border-black">
-            <section class="profile relative flex justify-center flex-col">
-                <div class="container w-1/3  bg-black border-4 border-black rounded-full mx-auto">
-                    <img class=" bg-slate-200 rounded-full" src="profile.png" alt="user" />
+        <div className={`resizable w-72 ${dark ? "bg-gray-800" : "bg-white"} font-poppins h-120 overflow-y-auto flex py-7 flex-col justify-between gap-4 items-center border-8 rounded-3xl ${dark ? "border-gray-500" : "border-gray-500"}`}>
+            <section className="profile relative flex justify-center flex-col">
+                <div className={`container w-1/3  ${!dark ? "bg-gray-800" : "bg-white"} border-4 border-black rounded-full mx-auto`}>
+                    <img className=" bg-slate-200 rounded-full" src="defaultPict.jpeg" alt="user" />
                 </div>
-                <div class="name mt-3">
-                    <h5 className="text-lg font-semibold">{titleProfile === "" ? "<Your Title>" : titleProfile}</h5>
+                <div className="name mt-3">
+                    <h5 className={`text-lg font-semibold ${dark ? "text-white" : "text-slate-700"}`}>{titleProfile === "" ? "<Your Title>" : titleProfile}</h5>
                 </div>
-                <div class="desc">
-                    <p className="text-sm">{descriptionProfile === "" ? "<Your Description>" : descriptionProfile}</p>
+                <div className="desc block">
+                    <p className={`text-sm block ${dark ? "text-white" : "text-slate-700"}`}>{descriptionProfile === "" ? "<Your Description>" : descriptionProfile}</p>
                 </div>
             </section>
-            <section class="main-content flex justify-center items-center flex-col w-52 gap-3 my-7">
+            <section className="main-content flex justify-center items-center flex-col w-52 gap-3 my-7">
                 {linksValue.map((link, index) => (
-                    <BubblePrototype key={index} link={link.link.link} title={link.link.title} />
+                    <BubblePrototype choosenTheme={choosenTheme} key={index} link={link.link.link} title={link.link.title} />
                 ))}
             </section>
             <footer>
-                <h1 className="text-sm font-semibold">myLinktree</h1>
+                <h1 className={`text-sm font-semibold ${dark ? "text-white" : "text-slate-700"}`}>myLinktree</h1>
             </footer>
         </div>
     );
