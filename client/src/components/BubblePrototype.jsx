9e4adc49a19abd45b2923878351b5343e54005ac
edricgalentino <erdrikpertama@gmail.com>
2022-06-16 11:52:31 +0700

    daw

--- client/src/components/BubblePrototype.jsx
+++ client/src/components/BubblePrototype.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-function BubblePrototype({ link, title }) {
+function BubblePrototype({ link, title, choosenTheme }) {
     return (
-        <div className={`bg-white w-full rounded-full p-1 relative hover:ml-2 hover:drop-shadow-xl transition-all ${link.length === 8 && link.startsWith("https://") ? "cursor-not-allowed" : ""}`}>
+        <div
+            className={`${choosenTheme.bgBubble === undefined ? "bg-emerald-400" : choosenTheme.bgBubble} w-full rounded-full p-1 relative hover:ml-2 hover:drop-shadow-xl transition-all ${
+                link.length === 8 && link.startsWith("https://") ? "cursor-not-allowed" : ""
+            }`}
+        >
             <a target="_blank" href={link} rel="noreferrer" className={`${link.length === 8 && link.startsWith("https://") ? "cursor-not-allowed pointer-events-none" : ""}`}>
-                <h6 className={`${title === "" ? "text-slate-500 text-sm" : "text-sm"} `}>{title === "" ? "enter your title..." : title}</h6>
+                <h6 className={`${choosenTheme.textColor} text-sm font-semibold`}>{title === "" ? "enter your title..." : title}</h6>
             </a>
         </div>
     );
