5da6f109e4817f1c57b159b288c1fb842c81f815
edricgalentino <erdrikpertama@gmail.com>
2022-06-17 14:53:05 +0700

    sad

---
+++ client/src/components/Background.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import SliderComponent from "./SliderComponent";
+import { BsFillMoonStarsFill, BsSun } from "react-icons/bs";
+function Background({ setChoosenTheme, isDark, setIsDark }) {
+    return (
+        <>
+            <h1 className=" text-lg text-left font-semibold">Available Themes</h1>
+            <div className="content w-full drop-shadow-xl rounded-xl bg-slate-100 items-center text-slate-700 flex flex-col gap-3 p-5 py-6 justify-start">
+                <div className="free flex justify-start items-start gap-1 flex-col">
+                    <div className="flex w-full gap-3 items-center justify-between">
+                        <span className=" text-lg text-left font-semibold">Free</span>
+                        <span
+                            className=" text-lg text-left flex items-center cursor-pointer font-semibold"
+                            onClick={() => {
+                                setIsDark(!isDark);
+                            }}
+                        >
+                            Mode : {!isDark ? <BsSun className="ml-3" /> : <BsFillMoonStarsFill className="ml-3" />}
+                        </span>
+                    </div>
+                    <div className="slider mb-8 lg:w-120 w-96">
+                        <SliderComponent dark={isDark} setChoosenTheme={(e) => setChoosenTheme(e)} />
+                    </div>
+                </div>
+                <div className="paid flex justify-start items-start gap-1 flex-col">
+                    <div className="flex w-full gap-3 items-center justify-between">
+                        <span className=" text-lg text-left font-semibold">Paid</span>
+                        <span
+                            className=" text-lg text-left flex items-center cursor-pointer font-semibold"
+                            onClick={() => {
+                                setIsDark(!isDark);
+                            }}
+                        >
+                            Mode : {!isDark ? <BsSun className="ml-3" /> : <BsFillMoonStarsFill className="ml-3" />}
+                        </span>
+                    </div>
+                    <div className="slider mb-8 lg:w-120 w-96">
+                        <SliderComponent dark={isDark} setChoosenTheme={(e) => setChoosenTheme(e)} />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Background;
